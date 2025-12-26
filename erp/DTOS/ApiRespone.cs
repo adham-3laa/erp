cf4ead7ca9c6043db8e1d8160d423db5f83782f4
adham-3la2 <adhamalaafouad_sd2024@fcis.bsu.edu.eg>
2025-12-26 16:39:10 +0200

    ش

--- erp/DTOS/ApiRespone.cs
+++ erp/DTOS/ApiRespone.cs
@@ -6,4 +6,13 @@
         public string message { get; set; } = "";
         public T value { get; set; } = default!;
     }
+    
+        public class ApiResponseForReturn<T>
+        {
+            public int StatusCode { get; set; }
+            public string Message { get; set; }
+            public string TraceId { get; set; }
+            public T Value { get; set; }
+        }
+    
 }
