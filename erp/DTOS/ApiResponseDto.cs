1580839cc32f96202cb3941ae84788cf677af5d3
Mazenzzx <mazenshreef55555@gmail.com>
2025-12-26 16:14:01 +0200

    تاني مرة يبهايم

--- erp/DTOS/ApiResponseDto.cs
+++ erp/DTOS/ApiResponseDto.cs
@@ -1,9 +1,9 @@
-﻿namespace erp.DTOS.Inventory.Responses
-{
-    public class ApiResponse<T>
-    {
-        public int statusCode { get; set; }
-        public string message { get; set; } = "";
-        public T value { get; set; } = default!;
-    }
-}
+﻿//namespace erp.DTOS.Inventory.Responses
+//{
+//    public class ApiResponse<T>
+//    {
+//        public int statusCode { get; set; }
+//        public string message { get; set; } = "";
+//        public T value { get; set; } = default!;
+//    }
+//}
