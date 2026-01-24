072f6aca4e90b9ae0ce597d8374f555c1edda128
adham-3la2 <adhamalaafouad_sd2024@fcis.bsu.edu.eg>
2026-01-24 15:41:28 +0200

    z

--- erp/ViewModels/CreateUserViewModel.cs
+++ erp/ViewModels/CreateUserViewModel.cs
@@ -20,11 +20,11 @@ namespace erp.ViewModels
 
             UserTypes = new ObservableCollection<UserTypeOption>
 {
-    new UserTypeOption("مدير النظام", "SystemAdmin"),
+    // new UserTypeOption("مدير النظام", "SystemAdmin"),
     new UserTypeOption("مستخدم", "User"),
     new UserTypeOption("عميل", "Customer"),
     new UserTypeOption("مندوب مبيعات", "SalesRep"),
-    new UserTypeOption("مدير مخزن", "StoreManager"),
+    // new UserTypeOption("مدير مخزن", "StoreManager"),
     new UserTypeOption("مورد", "Supplier"),
     new UserTypeOption("محاسب", "Accountant")
 };
